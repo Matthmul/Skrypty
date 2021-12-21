b8fe04cae18230d0716b9cc94f3d7ae35ee75dbc
Matthmul <73168857+Matthmul@users.noreply.github.com>
2021-12-21 19:58:17 +0100

    Zadanie 2 Chatbot v0.1

--- Zadanie2/actions/actions.py
+++ Zadanie2/actions/actions.py
@@ -7,21 +7,97 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import datetime
+import json
+
+class ActionGetDate(Action):
+    def name(self) -> Text:
+        return "action_get_date"
+
+    def run(self, dispatcher, tracker, domain):
+        f = open('opening_hours.json')
+        data = json.load(f)
+        f.close()    
+
+        response = """Restaurant open hour is: \n"""
+        
+        for date in data['items']:
+            open_time = data['items'][date]['open']
+            close_time = data['items'][date]['close']
+            if open_time != close_time:
+                response += """{} {} - {}.\n""".format(date, open_time, close_time)
+            else:
+                response += """{} restaurant is closed.\n""".format(date, open_time, close_time)
+
+        dispatcher.utter_message(response)
+        return []
+
+class ActionGetNowDate(Action):
+    def name(self) -> Text:
+        return "action_get_now_date"
+
+    def run(self, dispatcher, tracker, domain):
+        f = open('opening_hours.json')
+        data = json.load(f)
+        f.close()
+
+        date = datetime.datetime.now()
+        date_name = date.date().strftime("%A")
+        open_time = data['items'][date_name]['open']
+        close_time = data['items'][date_name]['close']
+
+        response = """Restaurant in {} open from {} to {}.""".format(date_name, open_time, close_time)
+
+        dispatcher.utter_message(response)
+        return []
+
+class ActionGetMenu(Action):
+    def name(self) -> Text:
+        return "action_get_menu"
+
+    def run(self, dispatcher, tracker, domain):
+        f = open('menu.json')
+        data = json.load(f)
+        f.close()
+        response = """Menu: \n"""
+
+        for item in data['items']:
+            name = item['name']
+            price = item['price']
+            preparation_time = item['preparation_time']
+            preparation_time_str = ""
+            if preparation_time >= 1:
+                preparation_time_str = str(preparation_time) + " h"
+            else:
+                preparation_time_str = str(preparation_time * 60) + " min"
+            response += """{} - price: {}$, estimated preparation time: {}.\n""".format(name, price, preparation_time_str)
+
+        dispatcher.utter_message(response)
+        return []
+
+class ActionMakeOrder(Action):
+    def name(self) -> Text:
+        return "action_get_order"
+
+    def run(self, dispatcher, tracker, domain):
+        print(tracker.latest_message['text'])
+        print(tracker.latest_message)
+        response = "Your order is:\n"
+        response += str(tracker.latest_message['text'])
+        dispatcher.utter_message(response)
+
+        dispatcher.utter_message("Are you sure that you want it?")
+        return []
+
+class ActionMakeOrder(Action):
+    def name(self) -> Text:
+        return "action_make_order"
+
+    def run(self, dispatcher, tracker, domain):
+
+        dispatcher.utter_message("Food orderd.")
+        return []
