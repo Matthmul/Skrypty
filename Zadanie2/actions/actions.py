3c533d66934f5a7bd251edaf3f2d2f602f6e2d2e
Matthmul <73168857+Matthmul@users.noreply.github.com>
2021-12-23 16:20:49 +0100

    Zadanie 2 Chatbot v0.5

--- Zadanie2/actions/actions.py
+++ Zadanie2/actions/actions.py
@@ -12,6 +12,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
+from rasa_sdk.events import AllSlotsReset
 import datetime
 import json
 
@@ -51,7 +52,7 @@ class ActionGetNowDate(Action):
         open_time = data['items'][date_name]['open']
         close_time = data['items'][date_name]['close']
 
-        response = """Restaurant in {} open from {} to {}.""".format(date_name, open_time, close_time)
+        response = """Restaurant in {} is open from {} to {}.""".format(date_name, open_time, close_time)
 
         dispatcher.utter_message(response)
         return []
@@ -86,18 +87,26 @@ class ValidateRestaurantForm(FormValidationAction):
         
     async def required_slots(self, slots_mapped_in_domain, dispatcher, tracker, domain):
         order_slot = tracker.slots.get("orderSlotek")
-        print("1")
-        print(order_slot)
-        print(slots_mapped_in_domain)
+        order_correctly = tracker.slots.get("order_correctly")
+        # print("1")
+        # print(order_slot)
+        # print(order_correctly)
+        # print(tracker.slots.get("order_stop"))
         if order_slot is not None:
-            return ["order_correctly"] + slots_mapped_in_domain
+            if order_correctly is False:
+                return ["order_stop", "order_correctly"] + slots_mapped_in_domain
+            else:
+                return ["order_correctly"] + slots_mapped_in_domain
         return slots_mapped_in_domain
 
     async def extract_orderSlotek(self, dispatcher, tracker, domain):
-        print("1.2")
+        # print("1.2")
         intent = tracker.get_intent_of_latest_message()
+        order_slot = tracker.slots.get("orderSlotek")
         if intent == "request_order":
             return {"orderSlotek": None}
+        elif order_slot is not None:
+            return {"orderSlotek": order_slot}
         else:
             return {"orderSlotek": tracker.latest_message['text']}
 
@@ -108,17 +117,41 @@ class ValidateRestaurantForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        print("1.3")
-        print(slot_value)
-        return {"orderSlotek": slot_value}
+        # print("1.3")
+        return {"orderSlotek": tracker.get_slot("orderSlotek")}
 
     async def extract_order_correctly(self, dispatcher, tracker, domain):
-        print("2")
+        # print("2")
         intent = tracker.get_intent_of_latest_message()
+        order_correctly = tracker.get_slot("order_correctly")
+        if order_correctly is not None:
+            return {"order_correctly": order_correctly}
         return {"order_correctly": intent == "affirm"}
 
     def validate_order_correctly(self, slot_value, dispatcher, tracker, domain):
-        print("3")
-        if tracker.get_slot("order_correctly"):
-            return {"orderSlotek": tracker.get_slot("orderSlotek"), "order_correctly": True}
-        return {"orderSlotek": None, "order_correctly": None}
+        # print("3")
+        return {"orderSlotek": tracker.get_slot("orderSlotek"), "order_correctly": tracker.get_slot("order_correctly")}
+
+    async def extract_order_stop(self, dispatcher, tracker, domain):
+        # print("4")
+        intent = tracker.get_intent_of_latest_message()
+        return {"order_stop": intent == "affirm"}
+
+    def validate_order_stop(self, slot_value, dispatcher, tracker, domain):
+        # print("5")
+        order_stop = tracker.get_slot("order_stop")
+        if not order_stop:
+            return {"orderSlotek": None, "order_correctly": None, "order_stop": None}
+        return {"orderSlotek": tracker.get_slot("orderSlotek"), "order_correctly": tracker.get_slot("order_correctly"), "order_stop": tracker.get_slot("order_stop")}
+
+class ActionGetMenu(Action):
+    def name(self) -> Text:
+        return "action_submit_order"
+
+    def run(self, dispatcher, tracker, domain):
+        if tracker.get_slot("order_stop") is not True:
+            # print("Ordered " + str(tracker.get_slot("orderSlotek")))
+            dispatcher.utter_message("Food orderd.")
+        else:
+            dispatcher.utter_message("Ordering canceled.")
+        return [AllSlotsReset()]
